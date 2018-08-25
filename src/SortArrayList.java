edb8d9b267fe7bc50d67f6c281523193c827696d
Starbuck <zoebuck25@gmail.com>
2018-08-25 12:49:37 +0100

    committing items
    Signed-off-by: Starbuck <zoebuck25@gmail.com>

--- src/SortArrayList.java
+++ src/SortArrayList.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SortArrayList {
+    public static void main (String args[]){
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(99);
+        numbers.add(5);
+        numbers.add(81);
+
+        Collections.sort(numbers);
+
+        System.out.println(numbers);  // prints [,81, 99]
+
+    }
+
 }
