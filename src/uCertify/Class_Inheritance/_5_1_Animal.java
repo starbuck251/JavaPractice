b041712261aecd2dc172bdaff590babd32fb8b89
Starbuck <zoebuck25@gmail.com>
2019-03-02 18:06:55 +0000

    Added class inheritence 5.1

---
+++ src/uCertify/Class_Inheritance/_5_1_Animal.java
@@ -0,0 +1,11 @@
+package uCertify.Class_Inheritance;
+
+public class _5_1_Animal {
+    private int age;
+
+    public int getAge(){ return age;}
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/uCertify/Class_Inheritance/_5_1_Lion.java
@@ -0,0 +1,10 @@
+package uCertify.Class_Inheritance;
+
+public class _5_1_Lion extends _5_1_Animal {
+    private void roar (){
+        System.out.println("The " +getAge() + " year old lion says: Roar!");
+        // the parents private age variable cannot be accessed here
+
+    }
+}
+
