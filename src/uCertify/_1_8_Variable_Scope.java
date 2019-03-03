c52afc315e5ba2d1ceade2e7c93a3f22cad59f9a
Starbuck <zoebuck25@gmail.com>
2019-03-03 11:29:16 +0000

    added 1.8 variable scope

---
+++ src/uCertify/_1_8_Variable_Scope.java
@@ -0,0 +1,31 @@
+package uCertify;
+
+public class _1_8_Variable_Scope {
+    static int classVariable = 5;
+    int instanceVariable = 10;
+
+    public void doStuff (int local){
+        System.out.println("local is available here only " + local);
+
+        System.out.println("class variable can be access here  " + classVariable);
+        System.out.println("instance variable can be access here " + instanceVariable);
+
+    }
+
+    public void main (String args []) {
+
+        System.out.println("local is not accessible here  " + local);
+        System.out.println("class variable can be access here  " + classVariable);
+        System.out.println("instance variable can be access here " + instanceVariable);
+
+        for (int i = 0; i <10; i++){
+            System.out.println("i is avail here" + i);
+        }
+
+        System.out.println("i is not avail here" + i);
+
+    }
+
+
+
+}
